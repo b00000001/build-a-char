cd41ac8f3c0e379dc0360ac228aed0e8c6b9f9a8
Andrew Bianco <andrewbianco98@gmail.com>
2021-06-27 15:05:42 -0400

    Created the login/out routes in User Routes, fix display issue with handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 // const { User } = require('../models');
 // const withAuth = require('../utils/auth');
 
-router.get("/", (req, res) => {
-	res.render("index");
+router.get("/login", (req, res) => {
+	res.render("login");
 });
 
 module.exports = router;
