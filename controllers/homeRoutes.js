577afcc4bc4527bbef3afb7c63d5488aeec8b0b6
Aaron Deas <deas.aaron@gmail.com>
2021-06-29 11:35:36 -0400

    Got list route to show correct page and pass user data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,62 +1,67 @@
 const { Character } = require("../models");
 
 const router = require("express").Router();
-// const { User } = require('../models');
+const { User } = require("../models");
 // const withAuth = require('../utils/auth');
 
 router.get("/", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 router.get("/login", () => {
-    if (req.session.logged_in) {
-        res.redirect("/");
-        return;
-    }
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
 });
 
 router.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 router.get("/list", async (req, res) => {
-    try {
-        const userData = await User.findAll({});
+  try {
+    const userData = await User.findAll({});
+    // const characterData = await Character.findAll({});
 
-        const users = userData.map((project) => project.get({ plain: true }));
+    const users = userData.map((project) => project.get({ plain: true }));
+    // const characters = characterData.map((character) =>
+    //   character.get({ plain: true })
+    // );
 
-        const currentUserData = await User.findByPk(req.session.user_id);
-        const user = currentUserData.get({ plain: true });
+    const currentUserData = await User.findByPk(req.session.user_id);
+    const user = currentUserData.get({ plain: true });
 
-        delete user.password;
+    delete user.password;
 
-        res.render("list", {
-            user,
-            users,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render("list", {
+      user,
+      //   characters,
+      users,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const dbCharaData = await Character.create({
-            name: req.body.name,
-            race: req.body.race,
-            gender: req.body.gender
-        });
+  try {
+    const dbCharaData = await Character.create({
+      name: req.body.name,
+      race: req.body.race,
+      gender: req.body.gender
+    });
 
-        req.session.save(() => {
-            req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.loggedIn = true;
 
-            res.status(200).json(dbCharaData);
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+      res.status(200).json(dbCharaData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
